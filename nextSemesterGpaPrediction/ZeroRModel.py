53f9d5c0980c6251bb5b21470501493c285edd3a
Henry <yangz@uwplatt.edu>
2019-06-13 15:44:58 -0500

    Code outputs basic dataset ready for training.
    Header: id prev term number current term number  prev GPA  current GPA

--- nextSemesterGpaPrediction/ZeroRModel.py
+++ nextSemesterGpaPrediction/ZeroRModel.py
@@ -5,20 +5,52 @@ import random
 
 if __name__== "__main__":
     __minTermNumber = 2
-    __combinedDataRange = None
+    termGPA = pd.read_csv('termGPA.csv', index_col="index")
+    headers = list(termGPA)
 
-    idNNumOfTerm = pd.read_csv("Book1.csv")
+    datasetForPairs = pd.DataFrame()
 
-    __combinedDataRange = idNNumOfTerm['numOfTerm'][1]
+    for id in headers:
+        terms = []
+        for termNumber in termGPA[id].index:
+            if termGPA[id].loc[termNumber] > 0:
+                terms.append(termNumber)
+        if len(terms) > 1:
+            randomTermIndex = random.randrange(1, len(terms))
+            secondTerm = terms[randomTermIndex]
+            firstTerm = terms[randomTermIndex - 1]
+            datasetForPairs[id] = [firstTerm, secondTerm]
 
-    for i in range(__combinedDataRange, 1, -1):
-        ids = np.zeros(0)
-        for j in range(idNNumOfTerm['numOfTerm'].size):
-            if  idNNumOfTerm['numOfTerm'][j] > 1:
-                ids = np.append(ids, random.randint(__minTermNumber, idNNumOfTerm['numOfTerm'][j]))
-                #ids = np.append(ids, random.randint(__minTermNumber, 1))
-        #del ids
-        print("")
+    finalDataSetHeaders = ['id', 'prev term number', 'current term number', 'prev GPA', 'current GPA']
+    finalDataSet = pd.DataFrame(columns=finalDataSetHeaders)
 
-    print(ids)
-    print()
+    ids = list(datasetForPairs)
+
+    for id in ids:
+        finalDataSet = finalDataSet.append({'id' : id, 'prev term number' : datasetForPairs[id][0],
+                                            'current term number' : datasetForPairs[id][1],
+                                            'prev GPA' : termGPA[id][datasetForPairs[id][0]],
+                                            'current GPA' : termGPA[id][datasetForPairs[id][1]]}, ignore_index=True)
+        # tempAdding = pd.DataFrame('id':[id])
+        # finalDataSet['id'] = finalDataSet['id'].append(tempAdding, ignore_index=False)
+        # del tempAdding
+        #
+        # tempAdding = pd.Series([datasetForPairs[id][0]], index = [id])
+        # finalDataSet['prev term number'] = finalDataSet['prev term number'].append(tempAdding, ignore_index=False)
+        # del tempAdding
+        #
+        # tempAdding = pd.Series([datasetForPairs[id][1]], index = [id])
+        # finalDataSet['current term number'] = finalDataSet['current term number'].append(tempAdding, ignore_index=False)
+        # del tempAdding
+        #
+        # tempAdding = pd.Series([termGPA[id][datasetForPairs[id][0]]], index = [id])
+        # finalDataSet['prev GPA'] = finalDataSet['prev GPA'].append(tempAdding, ignore_index=False)
+        # del tempAdding
+        #
+        # tempAdding = pd.Series([termGPA[id][datasetForPairs[id][1]]], index = [id])
+        # finalDataSet['prev GPA'] = finalDataSet['prev GPA'].append(tempAdding, ignore_index=False)
+        #
+        # tempAdding = pd.DataFrame('id':[id])
+        # del tempAdding
+
+    print(finalDataSet)
