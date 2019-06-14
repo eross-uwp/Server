ddd56d8c7094e8967b096889a2d3baddb0a9219d
Austin FitzGerald <fitzgeralaus@uwplatt.edu>
2019-06-14 11:23:07 -0500

    Updated ZeroR Data Set Generator. Accidentally overwrote it last commit.

--- nextSemesterGpaPrediction/BaseDataSetGenerator.py
+++ nextSemesterGpaPrediction/BaseDataSetGenerator.py
@@ -1,37 +1,56 @@
+import random
 import pandas as pd
 import zeroRModel as zrm
-import random
 
-if __name__ == "__main__":
-    termGPA = pd.read_csv('data\\termGPA.csv', index_col="index")  # our raw dataset
-    headers = list(termGPA)
+# CONSTANTS
+RAW_DATA_FILE = 'termGPA.csv'
+FINAL_DATA_FILE = 'finalDataSet.csv'
+FIRST_COLUMN = 'id'
+SECOND_COLUMN = 'prev term number'
+THIRD_COLUMN = 'current term number'
+FOURTH_COLUMN = 'prev GPA'
+FIFTH_COLUMN = 'current GPA'
+FINAL_DATA_FRAME_HEADERS = [FIRST_COLUMN, SECOND_COLUMN, THIRD_COLUMN, FOURTH_COLUMN, FIFTH_COLUMN]
 
-    datasetForPairs = pd.DataFrame()
 
-    for id in headers:  # for every student id in the raw dataset
+def get_term_pairs(raw_data):
+    headers = list(raw_data)
+
+    data_frame_for_pairs = pd.DataFrame()
+
+    for student_id in headers:  # for every student id in the raw dataset
         terms = []
-        for termNumber in termGPA[id].index:  # for every term row of an id column
-            if termGPA[id].loc[termNumber] > 0:
+        for termNumber in raw_data[student_id].index:  # for every term row of an id column
+            if raw_data[student_id].loc[termNumber] > 0:
                 terms.append(termNumber)  # copy all term numbers that relate to a non-zero gpa to a temp array
         if len(terms) > 1:  # as long as a student id is related to at least 2 terms of non-zero gpa
-            randomTermIndex = random.randrange(1, len(terms))
-            secondTerm = terms[randomTermIndex]
-            firstTerm = terms[randomTermIndex - 1]
-            datasetForPairs[id] = [firstTerm, secondTerm]  # save a random term and the term previous to it for each id
+            random_term_index = random.randrange(1, len(terms))
+            second_term = terms[random_term_index]
+            first_term = terms[random_term_index - 1]
+            data_frame_for_pairs[student_id] = [first_term, second_term]  # random term and the term previous to it for each id
+    return data_frame_for_pairs
 
-    finalDataSetHeaders = ['id', 'prev term number', 'current term number', 'prev GPA', 'current GPA']
-    finalDataSet = pd.DataFrame(columns=finalDataSetHeaders)
 
-    ids = list(datasetForPairs)
+def generate_final_dataset(term_pairs_data_frame, raw_data_frame):
+    final_data_frame = pd.DataFrame(columns=FINAL_DATA_FRAME_HEADERS)
+
+    ids = list(term_pairs_data_frame)
 
     # for each student id, copy the 2 terms we found and the corresponding GPAs to the final dataset
-    for id in ids:
-        finalDataSet = finalDataSet.append({'id': id, 'prev term number': datasetForPairs[id][0],
-                                            'current term number': datasetForPairs[id][1],
-                                            'prev GPA': termGPA[id][datasetForPairs[id][0]],
-                                            'current GPA': termGPA[id][datasetForPairs[id][1]]}, ignore_index=True)
+    for student_id in ids:
+        final_data_frame = final_data_frame.append({FIRST_COLUMN: student_id,
+                                                    SECOND_COLUMN: term_pairs_data_frame[student_id][0],
+                                                    THIRD_COLUMN: term_pairs_data_frame[student_id][1],
+                                                    FOURTH_COLUMN: raw_data_frame[student_id][
+                                                        term_pairs_data_frame[student_id][0]],
+                                                    FIFTH_COLUMN: raw_data_frame[student_id][
+                                                        term_pairs_data_frame[student_id][1]]}, ignore_index=True)
+    return final_data_frame
 
-    # print(finalDataSet)
-    finalDataSet.to_csv('data\\finalDataSet.csv', encoding='utf-8', index=False)
 
-    print(zrm.predict(finalDataSet['prev GPA']))
+if __name__ == "__main__":
+    rawData = pd.read_csv(RAW_DATA_FILE, index_col="index")  # our raw dataset
+    termPairsDataFrame = get_term_pairs(rawData)
+    finalDataFrame = generate_final_dataset(termPairsDataFrame, rawData)
+    finalDataFrame.to_csv(FINAL_DATA_FILE, encoding='utf-8', index=False)
+    print(zrm.predict(finalDataFrame[FOURTH_COLUMN]))
