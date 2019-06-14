09b6985b3022261745987db33539bb7852b422cb
Nate Braukhoff <braukhoff96@gmail.com>
2019-06-14 18:28:50 -0500

    Added Gradient Boosted Trees

---
+++ nextSemesterGpaPrediction/GradientBoostedTrees.py
@@ -0,0 +1,14 @@
+"""
+___authors___: Nate Braukhoff
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import ensemble
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+
+from scipy.special import expit
+
