4979a2faeca80f45aedfd3b34388e601d37a230e
Austin FitzGerald <fitzgeralaus@uwplatt.edu>
2019-06-17 14:09:03 -0500

    Organized LinearRegressionModel.py, made graphs more readable and auto-saving, stored results in text file.

--- nextSemesterGpaPrediction/LinearRegressionModel.py
+++ nextSemesterGpaPrediction/LinearRegressionModel.py
@@ -10,6 +10,11 @@ import pandas as pd
 import numpy as np
 import BaseDataSetGenerator as bd
 
+RESULTS_FOLDER = 'LinearRegressionResults\\'
+GRAPH_FILE_PREFIX = 'test'
+RESULTS_TEXTFILE = 'Results.txt'
+
+# Creating arrays that contain arrays holding the testing and training data. Reshaped to form a 1 row multi column array
 X_train = np.array([pd.read_csv('data\\test_train\\train_1.csv')['prev GPA'].values.reshape(-1, 1),
                     pd.read_csv('data\\test_train\\train_2.csv')['prev GPA'].values.reshape(-1, 1),
                     pd.read_csv('data\\test_train\\train_3.csv')['prev GPA'].values.reshape(-1, 1),
@@ -39,27 +44,35 @@ def lr_predict():
     np.random.seed(bd.RANDOM_SEED)
     model = LinearRegression()
 
-    ytests = []
-    ypreds = []
+    # hold all tests and predictions in order to calculate R^2 AND RMSE.
+    y_tests = []
+    y_preds = []
 
     for i in range(0, 5):
         # fitting the model and storing the predicted value from the test set
         model.fit(X_train[i], y_train[i])
         y_pred = model.predict(X_test[i])
 
-        ytests += list(y_test[i])  # the real value
-        ypreds += list(y_pred)  # the predicted value
+        y_tests += list(y_test[i])  # the real value
+        y_preds += list(y_pred)  # the predicted value
 
-        # graphing. x-axis is real prev term GPAs, y-axis is real current term GPAs, line is the linear regression
-        plt.scatter(X_test[i], y_test[i], color='g', )
-        plt.plot(X_test[i], model.predict(X_test[i]), color='k')
+        plt.scatter(X_test[i], y_test[i], color='g', label='real')  # the real data from the tests, in green
+        # plt.scatter(X_test[i], y_pred, color='r', label='predicted')  # the predicted data from the tests, in red
+        plt.plot(X_test[i], model.predict(X_test[i]), color='k', label='predicted')  # the linear regression line
         plt.title('test #' + str(i+1))
-        plt.show()
+        plt.xlabel('Prev term GPA')
+        plt.ylabel('Curr term GPA')
+        plt.legend(loc='upper left')
+        plt.savefig(RESULTS_FOLDER + GRAPH_FILE_PREFIX + str(i+1))  # saving graphs
+        plt.close()
 
     # Calculating the R^2 and RMSE from the actual curr-term GPAs and predicted curr-term GPAs
-    rr = metrics.r2_score(ytests, ypreds)
-    rmse_error = np.math.sqrt(metrics.mean_squared_error(ytests, ypreds))
-    print("R^2: {:.5f}%, RMSE: {:.5f}".format(rr * 100, rmse_error))
+    rr = metrics.r2_score(y_tests, y_preds)
+    rmse = np.math.sqrt(metrics.mean_squared_error(y_tests, y_preds)) / 4
+
+    # Saving the R^2 and RMSE to a text file.
+    with open(RESULTS_FOLDER + RESULTS_TEXTFILE, "w") as text_file:
+        text_file.write('R^2 = ' + str(rr) + ', RMSE = ' + str(rmse))
 
 
 if __name__ == "__main__":
