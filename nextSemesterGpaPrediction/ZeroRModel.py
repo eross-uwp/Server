4f5852b5e177e619c8f0fa70a57081ca8a61137a
Henry <zhiwei.yang1997@gmail.com>
2019-06-17 14:29:25 -0500

    Created calculation of RMSE of ZeroR Model.

--- nextSemesterGpaPrediction/ZeroRModel.py
+++
@@ -1,7 +0,0 @@
-def predict(original_data):
-    # Finds the mean of original_data
-    sum_of_gpa = 0
-    for row in original_data:
-        sum_of_gpa = sum_of_gpa + row
-    prediction = sum_of_gpa/original_data.size
-    return prediction
