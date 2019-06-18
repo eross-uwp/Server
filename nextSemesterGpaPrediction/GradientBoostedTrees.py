b27d7b5a81fe6b8f642ec5e70883cbcfd6a98852
Nate Braukhoff <braukhoff96@gmail.com>
2019-06-18 11:17:29 -0500

    Comments and restructure
    Added Comments to the script. Also created two methods that get the test and train data from git

--- nextSemesterGpaPrediction/GradientBoostedTrees.py
+++ nextSemesterGpaPrediction/GradientBoostedTrees.py
@@ -9,27 +9,46 @@ from sklearn.metrics import mean_squared_error
 import math
 import urllib.request
 
-NUM_TRAIN_TEST = 5  # 5 datasets
-TRAIN_DATA_PATH = 'https://raw.githubusercontent.com/earos-uwp/Server/master/nextSemesterGpaPrediction/data/test_train/train_'  # Raw file address
-TEST_DATA_PATH = 'https://raw.githubusercontent.com/earos-uwp/Server/master/nextSemesterGpaPrediction/data/test_train/test_'  # Raw file address
+NUM_TRAIN_TEST = 5  # 5 data sets
+# Raw file address
+TRAIN_DATA_PATH = 'https://raw.githubusercontent.com/earos-uwp/Server/master/nextSemesterGpaPrediction/data' \
+                  '/test_train/train_'
+# Raw file address
+TEST_DATA_PATH = 'https://raw.githubusercontent.com/earos-uwp/Server/master/nextSemesterGpaPrediction/data' \
+                 '/test_train/test_'
+
+
+# Getting training dataset from Github
+def get_train_data():
+    url = (TRAIN_DATA_PATH + str(x) + '.csv')
+    file = urllib.request.urlopen(url)
+
+    return pd.read_csv(file)
+
+
+# Getting testing dataset from Github
+def get_test_data():
+    url1 = (TEST_DATA_PATH + str(x) + '.csv')
+    file1 = urllib.request.urlopen(url1)
+
+    return pd.read_csv(file1)
+
 
 if __name__ == "__main__":
     for x in range(1, NUM_TRAIN_TEST + 1):
-        url = (TRAIN_DATA_PATH + str(x) + '.csv')
-        file = urllib.request.urlopen(url)
-        train = pd.read_csv(file)  # Getting training dataset from Github
+        train = get_train_data()
+        test = get_test_data()
 
-        url1 = (TEST_DATA_PATH + str(x) + '.csv')
-        file1 = urllib.request.urlopen(url1)
-        testData = pd.read_csv(file1)
-        # Getting testing dataset from Github
-
+        # set variables for the model
         x = train.drop('current GPA', 1)
         y = np.array(train['current GPA'])
 
+        # create the model
         model = GradientBoostingRegressor(n_estimators=500, learning_rate=0.01, max_features=1, max_depth=4, loss='ls')
         model.fit(x, y)
 
-        rmse_val = math.sqrt(mean_squared_error(testData['current GPA'].values, model.predict(testData.drop('current GPA', 1)))) / 4
+        # Calculate the Root mean squared
+        rmse_val = math.sqrt(mean_squared_error(test['current GPA'].values,
+                                                model.predict(test.drop('current GPA', 1)))) / 4
 
         print('RMSE = ' + str(rmse_val))
