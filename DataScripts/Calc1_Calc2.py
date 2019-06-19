461bc04a12315b2f3e6f0a1d28a3b4cbae97b02a
Nate Braukhoff <braukhoff96@gmail.com>
2019-06-19 14:51:17 -0500

    Fixed grammar error

--- DataScripts/Calc1_Calc2.py
+++ DataScripts/Calc1_Calc2.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     data = pd.read_csv('..\\Data\\Grades.csv')
 
     var = data[data.course_name.isin(['Calculus and Analytic Geometry I', 'Calculus and Analytic Geometry II'])][['student_id', 'course_name', 'grade']]
-    var.to_csv('J:\\Research Tools\\Research\\Server\\Data\\Gernerated_Pandas\\Calc1_Calc2.csv')
+    var.to_csv('J:\\Research Tools\\Research\\Server\\Data\\Generated_Pandas\\Calc1_Calc2.csv')
