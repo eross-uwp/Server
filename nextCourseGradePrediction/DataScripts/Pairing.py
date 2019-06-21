84dfcb4ffd3517477939cc550cce6bcceeafc895
Nate Braukhoff <braukhoff96@gmail.com>
2019-06-21 11:08:32 -0500

    Rename classes
    Rename class1 to classA and class2 to classB

--- nextCourseGradePrediction/DataScripts/Pairing.py
+++ nextCourseGradePrediction/DataScripts/Pairing.py
@@ -3,14 +3,5 @@ import pandas as pd
 if __name__ == "__main__":
     calc1 = pd.read_csv('..\\data\\Generated_Pandas\\class1.csv')
     calc2 = pd.read_csv('..\\data\\Generated_Pandas\\class2.csv')
-    pair = pd.DataFrame(columns=['student_id', 'class1', 'class2'])
+    pair = pd.DataFrame(columns=['student_id', 'classA1', 'ClassA2', 'ClassA3', 'classB'])
 
-    for i, row in calc1.iterrows():
-        studentId = calc1.at[i, 'student_id']
-        for j, row2 in calc2.iterrows():
-            if studentId == calc2.at[j, 'student_id']:
-                pair.at[i, 'student_id'] = studentId
-                pair.at[i, 'class1'] = calc1.at[i, 'grade']
-                pair.at[i, 'class2'] = calc2.at[j, 'grade']
-
-    pair.to_csv('..\\data\\Generated_Pandas\\classPairs.csv')
--- nextCourseGradePrediction/DataScripts/findDuplicates.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-if __name__ == '__main__':
-    class1 = pd.read_csv('..\\data\\Generated_Pandas\\class1.csv')
-
