f6e22bf9fb43f2d37c1b690f35da3542d15f5371
Evan <evanmjvan365@gmail.com>
2019-06-25 13:49:25 -0500

    Working script that gets the students grades.
    Working script that gets a students grades from each of their courses.  There is currently an error that it runs into at the end of the file, but otherwise should be working correctly.  Included is also the files that it has been working with.

--- nextCoursePredictionPrerequisites/findGradeForCourse.py
+++ nextCoursePredictionPrerequisites/findGradeForCourse.py
@@ -1,32 +1,39 @@
+"""
+___authors___: Evan Majerus
+"""
+
 import pandas as pd
-import numpy as np
 
 
 INDEX_MULTIPLIER = 3
 INDEX_OFFSET = 2
+COURSE = 'course_name'
+UNIQUE_COURSE = 'unique_courses'
+STUDENT_ID = 'student_id'
+GRADE = 'grade'
+RETAKE_COUNT = 'count'
 
 
 def grade_finder():
     count = 0
     for i, row in courses.iterrows():
-        reset_count()
         print(count)
         count = count + 1
         for j, tier in grades.iterrows():
-            if grades.at[j, 'course_name'] == courses.at[i, 'unique_courses']:
-                add_grade(grades.at[j, 'student_id'], grades.at[j, 'grade'], i)
+            if grades.at[j, COURSE] == courses.at[i, UNIQUE_COURSE]:
+                add_grade(grades.at[j, STUDENT_ID], grades.at[j, GRADE], i)
 
 
 def add_grade(id, grade, course_index):
-    index = students.loc[students['student_id'] == id].index[0]
+    index = students.loc[students[STUDENT_ID] == id].index[0]
     grade_index = course_index * INDEX_MULTIPLIER
-    grade_index = grade_index + INDEX_OFFSET + students.at[index, 'count']
-    students[students.columns[grade_index + 1]] = grade
-    students.at[index, 'count'] = students.at[index, 'count'] + 1
+    grade_index = grade_index + INDEX_OFFSET + students.at[index, RETAKE_COUNT]
+    students[students.columns[grade_index]][index] = grade
+    students.at[index, RETAKE_COUNT] = students.at[index, RETAKE_COUNT] + 1
 
 
 def reset_count():
-    students['count'] = 0
+    students[RETAKE_COUNT] = 0
 
 
 if __name__ == "__main__":
