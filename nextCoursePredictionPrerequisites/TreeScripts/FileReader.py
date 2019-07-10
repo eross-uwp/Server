8f751adc822cd7d7c1d8e4ae47fd327a87a307d9
Austin FitzGerald <fitzgeralaus@uwplatt.edu>
2019-07-10 13:28:04 -0500

    Fixed the FileReader. Works for se file now. need to comment and test.

--- nextCoursePredictionPrerequisites/TreeScripts/FileReader.py
+++ nextCoursePredictionPrerequisites/TreeScripts/FileReader.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from TreeScripts.Node import Node
 
+
 class FileReader:
     __SINGLE_RELATIONSHIP = 'SINGLE'
     __AND_RELATIONSHIP = 'AND'
@@ -12,29 +13,67 @@ class FileReader:
     __PREREQ = 'prereqs'
     __RELATIONSHIP = pd.read_csv('..\\data\\se.csv')
 
+    def get_commas_for_split(self, operator, items):
+        open_paren = 0
+        open_curly = 0
+        commas_needed = 1
+        char_iter = iter(items)
+        for idx, char in enumerate(char_iter):
+            if char == '(':
+                open_paren += 1
+                commas_needed += 1
+            if char == ')':
+                open_paren -= 1
+            if char == '{':
+                open_curly += 1
+            if char == '}':
+                open_curly -= 1
+            if idx > len(operator) and open_paren == 0:
+                return commas_needed
+
     def create_trees(self, postreq, items):
+        operator = items.split('(')[0]
+        items = items[(len(operator) + 1):-1]  # remove beginning operator and last parenthesis
+
         # if and/or
-        if items.split('(')[0] == self.__AND_RELATIONSHIP or items.split('(')[0] == self.__OR_RELATIONSHIP:
-            operator = items.split('(')[0]
-            nodes = [Node('', items.split('(')[0]), Node('', items.split('(')[0])]
+        if operator == self.__AND_RELATIONSHIP or operator == self.__OR_RELATIONSHIP:
+            commas_for_split = self.get_commas_for_split(operator, items)
+            item_1 = items.split(',', commas_for_split)
+            item_1 = ','.join(item_1[:commas_for_split])
+            item_2 = items.split(',', commas_for_split)
+            item_2 = ','.join(item_2[commas_for_split:])
+            nodes = [Node('', operator), Node('', operator)]
             postreq.add_prereq(nodes[0])
             postreq.add_prereq(nodes[1])
-            for idx,item in enumerate(items.split(',')):
-                removed_operator = item[(len(operator)+1):]
-                # if item is a class
-                if removed_operator[0] == '{':
-                    # get class name
-                    nodes[idx].set_grade(removed_operator.split('#')[1][:-1])
-                    nodes[idx].set_name(removed_operator[removed_operator.find('{')+1 : removed_operator.find(str('#') + nodes[idx].get_grade() + str('}'))])
-                    return self.create_trees(nodes[idx], self.find_items(nodes[idx].get_name()))
-                if item.split('(')[0] == self.__AND_RELATIONSHIP or item.split('(')[0] == self.__OR_RELATIONSHIP or item.split('(')[0] == self.__SINGLE_RELATIONSHIP:
-                    nodes[idx].set_name(random.randint(1,101))
-                    return self.create_trees(nodes[idx], item)
-                if item == '':
-                    return postreq
-        if items.split('(')[0] == self.__SINGLE_RELATIONSHIP:
+
+            # if item is a class
+            if item_1[0] == '{':
+                nodes[0].set_grade(item_1.split('#')[1][:-1])
+                nodes[0].set_name(item_1[item_1.find('{') + 1: item_1.find(
+                    str('#') + nodes[0].get_grade() + str('}'))])
+                self.create_trees(nodes[0], self.find_items(nodes[0].get_name()))
+
+            if item_1.split('(')[0] == self.__AND_RELATIONSHIP or item_1.split('(')[0] == self.__OR_RELATIONSHIP or \
+                    item_1.split('(')[0] == self.__SINGLE_RELATIONSHIP:
+                nodes[0].set_name(random.randint(1, 101))
+                self.create_trees(nodes[0], item_1)
+
+            if item_2[0] == '{':
+                nodes[1].set_grade(item_2.split('#')[1][:-1])
+                nodes[1].set_name(item_2[item_2.find('{') + 1: item_2.find(
+                    str('#') + nodes[1].get_grade() + str('}'))])
+                self.create_trees(nodes[1], self.find_items(nodes[1].get_name()))
+                return postreq
+
+            if item_2.split('(')[0] == self.__AND_RELATIONSHIP or item_2.split('(')[0] == self.__OR_RELATIONSHIP or \
+                    item_2.split('(')[0] == self.__SINGLE_RELATIONSHIP:
+                nodes[1].set_name(random.randint(1, 101))
+                self.create_trees(nodes[1], item_2)
+                return postreq
+
+        if operator == self.__SINGLE_RELATIONSHIP:
             a = Node('', self.__SINGLE_RELATIONSHIP)
-            removed_operator = items.split('}')[0][(len(self.__SINGLE_RELATIONSHIP) + 2):]
+            removed_operator = items.split('}')[0][1:]
             a.set_grade(removed_operator.split('#')[1])
             a.set_name(removed_operator.split('#')[0])
             postreq.add_prereq(a)
@@ -46,8 +85,8 @@ class FileReader:
                 return self.__RELATIONSHIP.at[i, self.__PREREQ]
         return ''
 
-    def do_stuff(self):
-        head_nodes = []
+    def do_stuff(self, head_nodes):
+
         for i, row in self.__RELATIONSHIP.iterrows():
             items = self.find_items(self.__RELATIONSHIP.at[i, self.__POSTREQ])
             test = items.split('(')[0]
@@ -58,5 +97,6 @@ class FileReader:
 if __name__ == "__main__":
     sys.setrecursionlimit(1908)
     aay = FileReader()
-    aay.do_stuff()
+    head_nodes = []
+    aay.do_stuff(head_nodes)
     print("DONE!")
