128d83a3348db1f669ea0d81e20f7405545015fa
Austin FitzGerald <fitzgeralaus@uwplatt.edu>
2019-07-10 16:30:17 -0500

    Fixed naming errors.

--- postrequisite_prediction/TreeScripts/TreeMaker.py
+++ postrequisite_prediction/TreeScripts/TreeMaker.py
@@ -10,20 +10,6 @@ import pandas as pd
 from TreeScripts.Node import Node
 
 
-def __get_commas_for_split(operator, items):
-    open_paren = 0
-    commas_needed = 1
-    char_iter = iter(items)
-    for idx, char in enumerate(char_iter):
-        if char == '(':
-            open_paren += 1
-            commas_needed += 1
-        if char == ')':
-            open_paren -= 1
-        if idx > len(operator) and open_paren == 0:
-            return commas_needed
-
-
 class TreeMaker:
     __SINGLE_RELATIONSHIP = 'SINGLE'
     __AND_RELATIONSHIP = 'AND'
@@ -43,7 +29,7 @@ class TreeMaker:
 
         # if and/or
         if operator == self.__AND_RELATIONSHIP or operator == self.__OR_RELATIONSHIP:
-            commas_for_split = get_commas_for_split(operator, items)
+            commas_for_split = self.__get_commas_for_split(operator, items)
             item_1 = items.split(',', commas_for_split)
             item_1 = ','.join(item_1[:commas_for_split])
             item_2 = items.split(',', commas_for_split)
@@ -57,24 +43,24 @@ class TreeMaker:
                 nodes[0].set_grade(item_1.split('#')[1][:-1])
                 nodes[0].set_name(item_1[item_1.find('{') + 1: item_1.find(
                     str('#') + nodes[0].get_grade() + str('}'))])
-                self.create_trees(nodes[0], self.find_items(nodes[0].get_name()))
+                self.__create_trees(nodes[0], self.__find_items(nodes[0].get_name()))
 
             if item_1.split('(')[0] == self.__AND_RELATIONSHIP or item_1.split('(')[0] == self.__OR_RELATIONSHIP or \
                     item_1.split('(')[0] == self.__SINGLE_RELATIONSHIP:
                 nodes[0].set_name(random.randint(1, 101))
-                self.create_trees(nodes[0], item_1)
+                self.__create_trees(nodes[0], item_1)
 
             if item_2[0] == '{':
                 nodes[1].set_grade(item_2.split('#')[1][:-1])
                 nodes[1].set_name(item_2[item_2.find('{') + 1: item_2.find(
                     str('#') + nodes[1].get_grade() + str('}'))])
-                self.create_trees(nodes[1], self.find_items(nodes[1].get_name()))
+                self.__create_trees(nodes[1], self.__find_items(nodes[1].get_name()))
                 return postreq
 
             if item_2.split('(')[0] == self.__AND_RELATIONSHIP or item_2.split('(')[0] == self.__OR_RELATIONSHIP or \
                     item_2.split('(')[0] == self.__SINGLE_RELATIONSHIP:
                 nodes[1].set_name(random.randint(1, 101))
-                self.create_trees(nodes[1], item_2)
+                self.__create_trees(nodes[1], item_2)
                 return postreq
 
         # if single relationship
@@ -84,7 +70,7 @@ class TreeMaker:
             a.set_grade(removed_operator.split('#')[1])
             a.set_name(removed_operator.split('#')[0])
             postreq.add_prereq(a)
-            return self.create_trees(a, self.find_items(a.get_name()))
+            return self.__create_trees(a, self.__find_items(a.get_name()))
 
     # returns the prereqs column cell that matches the given postreq
     def __find_items(self, class_name):
@@ -93,9 +79,22 @@ class TreeMaker:
                 return self.__READ_FILE.at[i, self.__PREREQ]
         return ''
 
+    def __get_commas_for_split(self, operator, items):
+        open_paren = 0
+        commas_needed = 1
+        char_iter = iter(items)
+        for idx, char in enumerate(char_iter):
+            if char == '(':
+                open_paren += 1
+                commas_needed += 1
+            if char == ')':
+                open_paren -= 1
+            if idx > len(operator) and open_paren == 0:
+                return commas_needed
+
     # get prereq tree for a given class
     def process(self, class_name):
         class_node = Node(class_name, 'SINGLE')
-        items = self.find_items(class_name)
-        self.create_trees(class_node, items)
+        items = self.__find_items(class_name)
+        self.__create_trees(class_node, items)
         return class_node
