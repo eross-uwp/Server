86e83c3bbb9d076961ad048de6f12f79687e5c6f
Austin FitzGerald <fitzgeralaus@uwplatt.edu>
2019-07-11 14:34:09 -0500

    Finished up failed_prereq_took_postreq_probability script. Outputs to file.

--- postrequisite_prediction/failed_prereq_took_postreq_probability.py
+++ postrequisite_prediction/failed_prereq_took_postreq_probability.py
@@ -8,6 +8,7 @@ import pandas as pd
 __COMBINED_STRUCTURE = 'data\\combined_structure.csv'
 __COMBINED_STRUCTURE_DATAFRAME = pd.read_csv(__COMBINED_STRUCTURE)
 __GRADES_WITH_TERM_DATAFRAME_ = pd.read_csv('data\\student_grade_list_with_terms.csv').fillna('')
+__OUTPUT_FILEPATH = 'results\\failed_prereq_took_postreq_probability.csv'
 
 
 def convert_grade(string_grade):
@@ -38,17 +39,57 @@ def convert_grade(string_grade):
         return 0
 
 
+def append_if_division_allowed(append_to, divide_this, by_this):
+    if by_this != 0:
+        append_to.append(divide_this / by_this)
+    else:
+        append_to.append(0)
+
+
 if __name__ == "__main__":
+    postreq_list = []
     tree_maker = TreeMaker(__COMBINED_STRUCTURE)
+
+    prereq_failed_postreq_taken_list = []
+    prereq_failed_postreq_not_taken_list = []
+    prereq_failed_postreq_passed_list = []
+    prereq_failed_postreq_passed_probability = []
+    prereq_failed_postreq_failed_list = []
+    prereq_failed_postreq_taken_probability = []
+
+    prereq_passed_postreq_taken_list = []
+    prereq_passed_postreq_not_taken_list = []
+    prereq_passed_postreq_passed_list = []
+    prereq_passed_postreq_passed_probability = []
+    prereq_passed_postreq_failed_list = []
+    prereq_passed_postreq_taken_probability = []
+
     for i, postreq_row in __COMBINED_STRUCTURE_DATAFRAME.iterrows():
-        prereq_failed_but_took_postreq = 0
-        prereq_failed_did_not_postreq = 0
-        prereq_failed_postreq_passed = 0
-        prereq_passed = 0
+        prereq_failed_postreq_taken_list.append(0)
+        prereq_failed_postreq_not_taken_list.append(0)
+        prereq_failed_postreq_passed_list.append(0)
+        prereq_failed_postreq_failed_list.append(0)
+
+        prereq_passed_postreq_taken_list.append(0)
+        prereq_passed_postreq_not_taken_list.append(0)
+        prereq_passed_postreq_passed_list.append(0)
+        prereq_passed_postreq_failed_list.append(0)
 
         postreq_with_children = tree_maker.process(postreq_row['postreq'])
+        postreq_list.append(postreq_with_children.get_name())
         prereqs = postreq_with_children.get_immediate_prereqs()
         for idx, student_row_series in __GRADES_WITH_TERM_DATAFRAME_.iterrows():
+            prereq_failed_postreq_taken = 0
+            prereq_failed_postreq_not_taken = 0
+            prereq_failed_postreq_passed = 0
+            prereq_failed_postreq_failed = 0
+
+            prereq_passed_postreq_taken = 0
+            prereq_passed_postreq_not_taken = 0
+            prereq_passed_postreq_passed = 0
+            prereq_passed_postreq_failed = 0
+
+            prereq_failed_took_postreq_prob = 0
             if postreq_with_children.get_name() in __GRADES_WITH_TERM_DATAFRAME_:
                 postreq_term_and_grade = __GRADES_WITH_TERM_DATAFRAME_[postreq_with_children.get_name()].values[idx]
                 for j, prereq in enumerate(prereqs):
@@ -57,17 +98,62 @@ if __name__ == "__main__":
                         prereq_grade = __GRADES_WITH_TERM_DATAFRAME_[prereq.get_name()].values[idx]
                         if prereq_grade != '':  # took prereq
                             prereq_grade = convert_grade(prereq_grade.split(',')[1])
-                            if prereq_grade <= prereq_grade_req:
-                                if postreq_term_and_grade != '':
-                                    prereq_failed_but_took_postreq += 1
-                                    if convert_grade(postreq_term_and_grade.split(',')[1]) >= convert_grade(postreq_with_children.get_grade()):
+                            if prereq_grade <= prereq_grade_req:  # failed prereq
+                                if postreq_term_and_grade != '':  # took postreq
+                                    prereq_failed_postreq_taken += 1
+                                    if convert_grade(postreq_term_and_grade.split(',')[1]) >= convert_grade(
+                                            postreq_with_children.get_grade()):  # postreq passed
                                         prereq_failed_postreq_passed += 1
-                                elif postreq_term_and_grade == '':
-                                    prereq_failed_did_not_postreq += 1
-                            else:
-                                prereq_passed += 1
-        print('Postreq: ' + postreq_with_children.get_name() + '\n' +
-              'Prereq failed first attempt but took postreq : ' + str(prereq_failed_but_took_postreq) + '\n' +
-              '     - passed postreq: ' + str(prereq_failed_postreq_passed) + '\n' +
-              'Prereq failed first attempt but did not take postreq: ' + str(prereq_failed_did_not_postreq) + '\n')
+                                    else:
+                                        prereq_failed_postreq_failed += 1
+                                elif postreq_term_and_grade == '':  # did not take postreq
+                                    prereq_failed_postreq_not_taken += 1
+                            else:  # passed prereq
+                                if postreq_term_and_grade != '':  # took postreq
+                                    prereq_passed_postreq_taken += 1
+                                    if convert_grade(postreq_term_and_grade.split(',')[1]) >= convert_grade(
+                                            postreq_with_children.get_grade()):  # postreq passed
+                                        prereq_passed_postreq_passed += 1
+                                    else:
+                                        prereq_passed_postreq_failed += 1
+                                elif postreq_term_and_grade == '':  # did not take postreq
+                                    prereq_passed_postreq_not_taken += 1
+            if prereq_failed_postreq_taken > 0:
+                prereq_failed_postreq_taken_list[i] += 1
+            if prereq_failed_postreq_not_taken > 0:
+                prereq_failed_postreq_not_taken_list[i] += 1
+            if prereq_failed_postreq_passed > 0:
+                prereq_failed_postreq_passed_list[i] += 1
+            if prereq_failed_postreq_failed > 0:
+                prereq_failed_postreq_failed_list[i] += 1
+            if prereq_passed_postreq_taken > 0:
+                prereq_passed_postreq_taken_list[i] += 1
+            if prereq_passed_postreq_not_taken > 0:
+                prereq_passed_postreq_not_taken_list[i] += 1
+            if prereq_passed_postreq_passed > 0:
+                prereq_passed_postreq_passed_list[i] += 1
+            if prereq_passed_postreq_failed > 0:
+                prereq_passed_postreq_failed_list[i] += 1
+        append_if_division_allowed(prereq_failed_postreq_passed_probability, prereq_failed_postreq_passed_list[i],
+                               prereq_failed_postreq_taken_list[i] + prereq_failed_postreq_not_taken_list[i])
+        append_if_division_allowed(prereq_failed_postreq_taken_probability, prereq_failed_postreq_taken_list[i],
+                                   prereq_failed_postreq_taken_list[i] + prereq_failed_postreq_not_taken_list[i])
+        append_if_division_allowed(prereq_passed_postreq_passed_probability, prereq_passed_postreq_passed_list[i],
+                               prereq_passed_postreq_taken_list[i] + prereq_passed_postreq_not_taken_list[i])
+        append_if_division_allowed(prereq_passed_postreq_taken_probability, prereq_passed_postreq_taken_list[i],
+                                   prereq_passed_postreq_taken_list[i] + prereq_passed_postreq_not_taken_list[i])
 
+    final_dataframe = pd.DataFrame(
+        {'Postreq': postreq_list, 'prereq_failed_postreq_taken': prereq_failed_postreq_taken_list,
+         'prereq_failed_postreq_not_taken': prereq_failed_postreq_not_taken_list,
+         'prereq_failed_postreq_passed': prereq_failed_postreq_passed_list,
+         'prereq_failed_postreq_passed_probability': prereq_failed_postreq_passed_probability,
+         'prereq_failed_postreq_failed': prereq_failed_postreq_failed_list,
+         'prereq_failed_postreq_taken_probability': prereq_failed_postreq_taken_probability,
+         'prereq_passed_postreq_taken': prereq_passed_postreq_taken_list,
+         'prereq_passed_postreq_not_taken': prereq_passed_postreq_not_taken_list,
+         'prereq_passed_postreq_passed': prereq_passed_postreq_passed_list,
+         'prereq_passed_postreq_passed_probability': prereq_passed_postreq_passed_probability,
+         'prereq_passed_postreq_failed': prereq_passed_postreq_failed_list,
+         'prereq_passed_postreq_taken_probability': prereq_passed_postreq_taken_probability})
+    final_dataframe.to_csv(__OUTPUT_FILEPATH)
