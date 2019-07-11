a786b942e081e5020b8653d34d67d541aa2678b8
Evan <evanmjvan365@gmail.com>
2019-07-11 14:08:41 -0500

    Added more to the prerequisite linear regression model.
    Also made a small change to the cumulative gpa csv.

--- postrequisite_prediction/TreeScripts/PostreqLogisticRegressionModel.py
+++ postrequisite_prediction/TreeScripts/PostreqLogisticRegressionModel.py
@@ -6,25 +6,55 @@ from TreeScripts.Node import Node
 
 
 class PostreqLogisticRegressionModel:
+    __STUDENT_ID = 'student_id'
+    __CUMULATIVE_GPA = 'cumulative_gpa'
+    __PREV_TERM_GPA = 'prev_term_gpa'
+    __STRUGGLE = 'struggle'
+    __EXTREME_STRUGGLE = 'extreme_struggle'
+    __TERM_DIFFERENCE = 'term_difference'
+
     def __create_data_frame(self , tree, grades):
         postrequisite = tree.get_name()
-        data_frame = pd.DataFrame(columns=['student_id', postrequisite])
+        data_frame = pd.DataFrame(columns=[self.__STUDENT_ID, postrequisite])
         prerequisite = tree.get_prereq()
         for j in prerequisite:
-            data_frame[prerequisite[j].get_name]
-        data_frame['struggle', 'extreme_struggle', 'term_difference']
+            data_frame[prerequisite[j].get_name()] = ''
+        data_frame[self.__CUMULATIVE_GPA] = ''
+        data_frame[self.__PREV_TERM_GPA] = ''
+        data_frame[self.__STRUGGLE] = ''
+        data_frame[self.__EXTREME_STRUGGLE] = ''
+        data_frame[self.__TERM_DIFFERENCE] = ''
+        data_frame = self.__get_student_info(data_frame, grades, postrequisite, prerequisite)
         return data_frame
+
+    def __get_student_info(self, data_frame, grades, postrequisite, prerequisite):
+        data_frame_row = 1
+        for j, tier in grades.iterrows():
+            if grades.at[j, postrequisite] != '' and self.__taken_prereq(j, grades, prerequisite):
+                data_frame.at[data_frame_row, self.__STUDENT_ID] = grades.at[j, self.__STUDENT_ID].split(',')[1]
+                postreq_term = grades.at[j, self.__STUDENT_ID].split(',')[0]
+                earliest_term = 2000
+                for k in prerequisite:
+                    if grades.at[j, prerequisite[k]] != '':
+                        data_frame.at[data_frame_row, prerequisite[k]] = grades.at[j, prerequisite[k]].split(',')[1]
+                        if grades.at[j, self.__STUDENT_ID].split(',')[0] < earliest_term:
+                            earliest_term = grades.at[j, self.__STUDENT_ID].split(',')[0]
+                data_frame.at[data_frame_row, self.__TERM_DIFFERENCE] = postreq_term - earliest_term
+                
+                data_frame_row = data_frame_row + 1
+        return data_frame
+
+    def __taken_prereq(self, index, grades, prerequisite):
+        for k in prerequisite:
+            if grades.at[index, prerequisite[k]] != '':
+                return True
+        return False
 
 
 if __name__ == "__main__":
-    prerequisites = pd.read_csv('..\\data\\combined_structure.csv')
+    structure = pd.read_csv('..\\data\\combined_structure.csv')
     grades = pd.read_csv('..\\data\\student_grade_list.csv')
-    prerequisite_tree_maker = TreeMaker(prerequisites)
-    for i, row in prerequisites.iterrows():
-        # data_frame = pd.DataFrame(columns = ['student_id', 'pre_one', 'pre_one_grade', 'pre_two', 'pre_two_grade',
-                                            # 'pre_three', 'pre_three_grade', 'pre_four', 'pre_four_grade',
-                                            # 'cumulative_gpa', 'prev_term_gpa', 'struggle', 'extreme_struggle',
-                                            # 'postreq', 'postreg_grade', 'term_difference'])
+    prerequisite_tree_maker = TreeMaker(structure)
+    for i, row in structure.iterrows():
         tree = prerequisite_tree_maker.process(row['postreq'])
         data_frame = PostreqLogisticRegressionModel.__create_data_frame(tree, grades)
-        PostreqLogisticRegressionModel.__get_student_info(data_frame, grades)
