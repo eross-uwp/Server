48e52776144dbf9d91b39268fe67d17fc0039ca5
Evan <evanmjvan365@gmail.com>
2019-07-11 12:50:03 -0500

    Small changes made to Postreq Logistic Regression Model.

--- postrequisite_prediction/TreeScripts/PostreqLogisticRegressionModel.py
+++ postrequisite_prediction/TreeScripts/PostreqLogisticRegressionModel.py
@@ -8,25 +8,25 @@ class PostreqLogisticRegressionModel:
 
     def __get_course_info(self, tree, grades):
         postrequisite = tree.get_name()
+        data_frame = pd.DataFrame(columns = ['student_id', postrequisite])
         prerequisite = tree.get_prereq()
         prerequisite_names = []
         pre_grades = []
-        for j in prerequisite:
-            prerequisite_names.append(prerequisite[j].get_name())
-        for j, tier in grades.iterrows():
-            if tier[postrequisite] != '':
-                post_grade = tier[postrequisite]
-            for k in prerequisite_names:
-                pre_grades.append(tier[prerequisite_names[k]])
-            if check_prerequisites(pre_grades):
+        
 
+        return data_frame
 
-    def __
+    def __get_if_struggled(self, pre_grades):
+
 
 if __name__ == "__main__":
-    prerequisites = pd.read_csv('..\\data\\combined.csv')
+    prerequisites = pd.read_csv('..\\data\\combined_structure.csv')
     grades = pd.read_csv('..\\data\\student_grade_list.csv')
     prerequisite_tree_maker = TreeMaker(prerequisites)
     for i, row in prerequisites.iterrows():
+        # data_frame = pd.DataFrame(columns = ['student_id', 'pre_one', 'pre_one_grade', 'pre_two', 'pre_two_grade',
+                                            # 'pre_three', 'pre_three_grade', 'pre_four', 'pre_four_grade',
+                                            # 'cumulative_gpa', 'prev_term_gpa', 'struggle', 'extreme_struggle',
+                                            # 'postreq', 'postreg_grade', 'term_difference'])
         tree = prerequisite_tree_maker.process(row['postreq'])
-        PostreqLogisticRegressionModel.get_course_info(tree, grades)
+        data_frame = PostreqLogisticRegressionModel.get_course_info(tree, grades, data_frame)
