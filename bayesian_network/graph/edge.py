86b284138562c6699a7eaab6048e1f088e4f260e
Nate <braukhoff96@gmail.com>
2019-07-14 14:46:13 -0500

    Edges
    The edge class is finished

--- bayesian_network/graph/edge.py
+++ bayesian_network/graph/edge.py
@@ -11,8 +11,8 @@ class Edge:
     def __init__(self, parent, child):
         self.edge = [parent, child]
 
-    def get_source(self):
+    def get_parent(self):
         return self.edge[0]
 
-    def get_destination(self):
+    def get_child(self):
         return self.edge[1]
--- bayesian_network/graph/tests/test_edge.py
+++ bayesian_network/graph/tests/test_edge.py
@@ -1,10 +1,21 @@
 from unittest import TestCase
 from edge import Edge
+from node import Node
 
 
 class TestEdge(TestCase):
-    def test_get_source(self):
-        e = Edge()
+    def test_get_parent(self):
+        a = Node('A')
+        b = Node('B')
+
+        e = Edge(a, b)
+
+        self.assertTrue('A' == e.get_parent().get_name())
 
     def test_get_destination(self):
-        self.fail()
+        a = Node('A')
+        b = Node('B')
+
+        e = Edge(a, b)
+
+        self.assertTrue('B' == e.get_child().get_name())
