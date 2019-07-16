4db33c4a6e0d072cc988e3c9faafdca86ac4386a
Nate Braukhoff <braukhoff96@gmail.com>
2019-07-16 13:15:54 -0500

    Knowledge Base Data
    Completed the methods that will interact with the class data for every node.

--- bayesian_network/bayesian_network/knowledge_base.py
+++ bayesian_network/bayesian_network/knowledge_base.py
@@ -24,6 +24,33 @@ class KnowledgeBase:
     def get_relations(self):
         return self._relations
 
+    def get_relation(self, class_name):
+        """
+        get_relation will return the relation that the Node has with another Node. If no relation exists then the
+        method will return None
+        :param class_name: String
+        :return: String or None
+        """
+        # Todo need to implement this method when Node relation comes in play
+        return self
+
+    def get_class_data(self, class_name_list):
+        """
+        get_class_data will return a Data Frame that contains all data for each class in the class_name_list. If a class
+        doesn't exist in _data will not return anything for that class. If all classes don't exist in _data then this
+        method will return None
+        :param class_name_list: List of Strings
+        :return: Data Frame or None
+        """
+        valid_name = []
+        for name in class_name_list:
+            if name in self._data.columns:
+                valid_name.append(name)
+        if len(valid_name) != 0:
+            return self._data[valid_name]
+        else:
+            return None
+
     def add_data(self, data_file_path):
         """
         add_data will add columns to the bayesian network's DataFrame
@@ -41,10 +68,17 @@ class KnowledgeBase:
         self._data.update(df)
 
     def update_relation(self, relation_file_path):
-        return None
+        """
+        update an existing relation between Nodes
+        :param relation_file_path: String
+        """
+        # Todo need to implement
+        return self
 
     def add_relation(self, relation_file_path):
-        return None
-
-    def get_class_data(self, class_list):
-        return None
+        """
+        Adds relations between Nodes
+        :param relation_file_path: String
+        """
+        # Todo need to implement
+        return self
---
+++ bayesian_network/bayesian_network/tests/test_knowledgeBase.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from knowledge_base import KnowledgeBase
+
+
+class TestKnowledgeBase(TestCase):
+    def test_get_data(self):
+        kb = KnowledgeBase(None, '..\\..\\test_data\\Test_Data_AB.csv')
+        print(kb.get_data())
+
+    def test_add_data(self):
+        kb = KnowledgeBase(None, '..\\..\\test_data\\Test_Data_AB.csv')
+        print(kb.get_data())
+        print()
+        kb.add_data('..\\..\\test_data\\Test_Data_C.csv')
+        print(kb.get_data())
+
+    def test_update_data(self):
+        kb = KnowledgeBase(None, '..\\..\\test_data\\Test_Data_AB.csv')
+        print(kb.get_data())
+        print()
+        kb.update_data('..\\..\\test_data\\test_data_A_update.csv')
+        print(kb.get_data())
+
+    def test_get_class_data(self):
+        kb = KnowledgeBase(None, '..\\..\\test_data\\Test_Data_AB.csv')
+        kb.add_data('..\\..\\test_data\\Test_Data_C.csv')
+        print(kb.get_data())
+        print()
+
+        print(kb.get_class_data(['A', 'C']))
+        print()
+        print(kb.get_class_data(['B']))
+        print(kb.get_class_data(['Z']))
+        print(kb.get_class_data(['A', 'B', 'Z']))
+
