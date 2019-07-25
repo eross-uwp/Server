82781763502fcfbf6265bcc5b99701d58af3e847
Nate Braukhoff <braukhoff96@gmail.com>
2019-07-25 12:20:30 -0500

    Fully Tested The CPT class

--- bayesian_network/bayesian_network/conditional_probability_table.py
+++ bayesian_network/bayesian_network/conditional_probability_table.py
@@ -4,31 +4,51 @@ import pandas as pd
 
 
 class ConditionalProbabilityTable:
-    def __init__(self, data):
-        self._data = data
-        self._cpt = pd.DataFrame()
+    def __init__(self, owner_name, parent_list=None, parents_data=None):
+        """
+        Constructs a Conditional Probability Table
+        :param owner_name: String 
+        :param parent_list: list of Strings
+        :param parents_data: DataFrame
+        """
+        if parents_data is None:
+            parents_data = pd.DataFrame()
+        if parent_list is None:
+            parent_list = []
 
-    # todo implement all methods from the Node class
+        self._parents_data = parents_data
+        self._cpt = pd.DataFrame()
+        self._parent_list = parent_list
+        self._owner = owner_name
+
     def get_table(self):
         return self._cpt
 
-    def get_all_combination(self, l, number_of_parents):
+    def get_parent_list(self):
+        return self._parent_list
+
+    def get_owner_name(self):
+        return self._owner
+
+    def add_parents_data(self, parents_data):
+        self._parents_data = parents_data
+        self._parent_list = list(parents_data.columns)
+
+    def get_all_combination(self, l):
         """
         get_all_combination takes in a list of items, then returns all possible combinations of each item.
-        :param number_of_parents: int
         :param l: list of items
         :return: list of combinations
         """
         combination_list = []
-        for i in list(itertools.product(l, repeat=number_of_parents)):
+        for i in list(itertools.product(l, repeat=len(self._parent_list))):
             combination_list.append(i)
 
         return combination_list
 
-    def get_combination_probability(self, filtered_data, predict, name_of_node):
+    def get_combination_probability(self, filtered_data, predict):
         """
         get_combination_probability will return the probability of the node's state given it's parents' states.
-        :param name_of_node: string
         :param filtered_data: DataFrame
         :param predict: state of Node
         :return: float
@@ -36,68 +56,64 @@ class ConditionalProbabilityTable:
         number_of_rows = filtered_data.shape[0]
         if number_of_rows == 0:
             return 0
-        occurrence = len(filtered_data[filtered_data[name_of_node] == predict])
+        occurrence = len(filtered_data[filtered_data[self._owner] == predict])
 
         return occurrence / number_of_rows
 
-    def update_cp_table(self, data, scale, number_of_parents, name):
+    def update_cp_table(self, data, scale):
         """
         update_probability_table will up a DataFrame that will represent the Node's Conditional Probability Table
         (CP Table). This DataFrame will contain all of the probability of the Node's states given the parent's states.
-        :param name: string
-        :param number_of_parents: integer
         :param data: DataFrame
         :param scale: List of strings
         """
         cpt = self.create_data_frame_columns(scale)
         # When a Node has no parents then filtering based on parent states is not needed.
-        if len(number_of_parents) == 0:
+        if len(self._parent_list) == 0:
             row = []
             for i in range(0, len(scale)):
-                row.append(self.get_combination_probability(data, scale[i], name))
+                row.append(self.get_combination_probability(data, scale[i]))
             cpt.loc[0] = row
 
             self._cpt = cpt
         else:
-            combo = self.get_all_combination(scale, number_of_parents)
+            combo = self.get_all_combination(scale)
 
             for i in range(0, len(combo)):
                 row = []
                 row.extend(combo[i])
                 for value in scale:
-                    row.append(self.get_combination_probability(self.filter_data(data, combo[i]), value, name))
+                    row.append(self.get_combination_probability(self.filter_data(data, combo[i]), value))
                 cpt.loc[i] = row
 
             self._cpt = cpt
 
-    def create_data_frame_columns(self, scale, parents):
+    def create_data_frame_columns(self, scale):
         """
         create_data_frame_columns will add all the node's parent's names as columns in the DataFrame. After the parent's
         each value in the scale will also be a column after the parents name.
-        :param parents: list
         :param scale: list of Strings
         :return: DataFrame
         """
         columns = []
 
-        if len(parents) != 0:
-            for parent in parents:
-                columns.append(parent.get_name())
+        if len(self._parent_list) != 0:
+            for parent in self._parent_list:
+                columns.append(parent)
 
         columns.extend(scale)
 
         return pd.DataFrame(columns=columns)
 
-    def filter_data(self, data, combination, parents):
+    def filter_data(self, data, combination):
         """
         filter_data will filter the data frame based on the combination of the parents states.
-        :param parents:
         :param data: Data Frame. Pre condition: Node must have at least one parent
         :param combination: list of strings
         :return: Data Frame
         """
         df = data
 
-        for i in range(0, len(parents)):
-            df = df[df[parents[i].get_name()] == combination[i]]
+        for i in range(0, len(self._parent_list)):
+            df = df[df[self._parent_list[i]] == combination[i]]
         return df
--- bayesian_network/bayesian_network/tests/test_conditionalProbabilityTable.py
+++ bayesian_network/bayesian_network/tests/test_conditionalProbabilityTable.py
@@ -1,71 +1,80 @@
 from unittest import TestCase
 from node import Node
+from conditional_probability_table import ConditionalProbabilityTable
+import pandas as pd
 
 
 class TestConditionalProbabilityTable(TestCase):
     def test_get_table(self):
         self.fail()
 
+    def test_get_parent_list(self):
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt = ConditionalProbabilityTable('Success', ['Work Hard', 'Smart'], test_data)
+        print(cpt.get_parent_list())
+
+    def test_get_owner(self):
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt = ConditionalProbabilityTable('ClassA')
+        self.assertTrue('ClassA' == cpt.get_owner_name())
+
     def test_get_all_combination(self):
-        a = Node('A')
-        b = Node('B')
-        c = Node('C')
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt = ConditionalProbabilityTable('ClassA', ['Success', 'Hard Work', 'Smart'], test_data)
+        cpt1 = ConditionalProbabilityTable('ClassA', ['Hard Work', 'Smart'], test_data)
 
-        a.add_parent(b)
-        a.add_parent(c)
+        self.assertTrue(8 == len(cpt.get_all_combination(['T', 'F'])))
+        print(cpt.get_all_combination(['T', 'F']))
 
-        self.assertTrue(4 == len(a.get_all_combination(['T', 'F'])))
-        print(a.get_all_combination(['T', 'F']))
+        self.assertTrue(125 == len(cpt.get_all_combination(['A', 'B', 'C', 'D', 'F'])))
+        print(cpt.get_all_combination(['A', 'B', 'C', 'D', 'F']))
 
-        self.assertTrue(25 == len(a.get_all_combination(['A', 'B', 'C', 'D', 'F'])))
-        print(a.get_all_combination(['A', 'B', 'C', 'D', 'F']))
+        self.assertTrue(4 == len(cpt1.get_all_combination(['T', 'F'])))
+        print(cpt1.get_all_combination(['T', 'F']))
 
-    def test_get_combination_probability(self):
-        a = Node('Smart')
-        b = Node('Work Hard')
-        c = Node('Success')
-
-        c.add_parent(a)
-        c.add_parent(b)
-
-        test_data = pd.read_csv('..\\..\\..\\test_data\\doug_example.csv')
-        self.assertTrue(.8 == c.get_combination_probability(c.filter_data(test_data, ['T', 'T']), 'T'))
-        self.assertTrue(0 == c.get_combination_probability(c.filter_data(test_data, ['T', 'I']), 'T'))
-
-    def test_update_cp_table(self):
-        a = Node('Smart')
-        b = Node('Work Hard')
-        c = Node('Success')
-
-        c.add_parent(a)
-        c.add_parent(b)
-
-        test_data = pd.read_csv('..\\..\\..\\test_data\\doug_example.csv')
-
-        c.update_cp_table(test_data, ['T', 'F'])
-        print(c.get_cp_table())
-
-        a.update_cp_table(test_data, ['T', 'F'])
-        print(a.get_cp_table())
-
-        b.update_cp_table(test_data, ['T', 'F'])
-        print(b.get_cp_table())
-
-    def test_create_data_frame_columns(self):
-        self.fail()
+        self.assertTrue(25 == len(cpt1.get_all_combination(['A', 'B', 'C', 'D', 'F'])))
+        print(cpt1.get_all_combination(['A', 'B', 'C', 'D', 'F']))
 
     def test_filter_data(self):
-        a = Node('Smart')
-        b = Node('Work Hard')
-        c = Node('Success')
-        test_data = pd.read_csv('..\\..\\..\\test_data\\doug_example.csv')
-
-        c.add_parent(a)
-        c.add_parent(b)
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt = ConditionalProbabilityTable('Success', ['Smart', 'Work Hard'], test_data)
 
         combo = ['T', 'T']
 
-        print(c.filter_data(test_data, combo))
+        print(cpt.filter_data(test_data, combo))
 
         combo = ['F', 'T']
-        print(c.filter_data(test_data, combo))
+        print(cpt.filter_data(test_data, combo))
+
+    def test_get_combination_probability(self):
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt = ConditionalProbabilityTable('Success', ['Smart', 'Work Hard'], test_data)
+
+        self.assertTrue(.8 == cpt.get_combination_probability(cpt.filter_data(test_data, ['T', 'T']), 'T'))
+        self.assertTrue(0 == cpt.get_combination_probability(cpt.filter_data(test_data, ['T', 'I']), 'T'))
+
+    def test_create_data_frame_columns(self):
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt = ConditionalProbabilityTable('Success', ['Smart', 'Work Hard'], test_data)
+        print(cpt.create_data_frame_columns(['T', 'F']))
+
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt1 = ConditionalProbabilityTable('Smart', None, test_data)
+        print(cpt1.create_data_frame_columns(['T', 'F']))
+
+    def test_update_cp_table(self):
+        test_data = pd.read_csv('..\\..\\test_data\\doug_example.csv')
+        cpt = ConditionalProbabilityTable('Success', ['Smart', 'Work Hard'], test_data)
+
+        cpt.update_cp_table(test_data, ['T', 'F'])
+        print(cpt.get_table())
+
+        cpt1 = ConditionalProbabilityTable('Work Hard', None, test_data)
+        cpt1.update_cp_table(test_data, ['T', 'F'])
+        print(cpt1.get_table())
+
+        cpt2 = ConditionalProbabilityTable('Smart', None, test_data)
+        cpt2.update_cp_table(test_data, ['T', 'F'])
+        print(cpt2.get_table())
+
+
