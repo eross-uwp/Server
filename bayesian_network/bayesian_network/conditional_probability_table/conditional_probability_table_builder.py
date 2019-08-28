8bf7dba72882c02c1618e16d20bf6b9e21e5cdad
Nate Braukhoff <braukhoff96@gmai.com>
2019-08-28 12:52:10 -0500

    implemented building a CPT when a node has no parents.

--- bayesian_network/bayesian_network/conditional_probability_table/conditional_probability_table_builder.py
+++ bayesian_network/bayesian_network/conditional_probability_table/conditional_probability_table_builder.py
@@ -16,19 +16,35 @@ class CPTBuilder:
         columns = list(data.columns.values)
 
         self._owner = columns[0]
+        columns.remove(self._owner)
+
         self._parents = columns
         self._owner_data = data
         self._scale = scale
 
-        columns.remove(self._owner)
-
     def build_with_no_parents(self):
+        """
+        This method will build the Conditional Probability Table for a Node that has no parents
+        :return: DataFrame
+        """
         table = self._make_columns()
-        row = []
-        for i in range(0, len(self._scale)):
-            # get probability
-            print()
-        table.loc[0] = row
+        table.loc[0] = self._add_probabilities_to_row(list(self._owner_data.values))
+        return table
+
+    def build_with_parents(self):
+        table = self._make_columns()
+        combinations = self._get_all_possible_combination()
+
+        for i in range(0, len(combinations)):
+            row = []
+            combination = combinations[i]
+
+            row.extend(combination)
+            entry_list = list(self._apply_combination_filter(combination))
+            row.extend(self._add_probabilities_to_row(entry_list))
+
+            table.loc[i] = row
+        return table
 
     def _make_columns(self):
         """
@@ -37,13 +53,20 @@ class CPTBuilder:
         """
         columns = copy.deepcopy(self._parents)
         columns.extend(self._scale)
-
         return pd.DataFrame(columns=columns)
 
-    def calculate_probability(self, combination, prediction):
-        print()
-        # apply filter with combination
-        # count predictions in owner column
+    def _calculate_probability(self, entry_list, prediction):
+        """
+        This method calculate the probability of the owner's state based on the states of it's parents.
+        :param entry_list: list
+        :param prediction: char
+        :return:
+        """
+        total_entries = len(entry_list)
+        if total_entries == 0:  # change to shape[0] >= 20
+            return 0
+        occurrence = entry_list.count(prediction)
+        return occurrence / total_entries
 
     def _apply_combination_filter(self, combination):
         """
@@ -55,7 +78,31 @@ class CPTBuilder:
         df = self._owner_data
         for i in range(0, len(self._parents)):
             temp_df = df[df[self._parents[i]] == combination[i]]
-            if temp_df.shape[0] == 0:  # change to shape[0] >= 20
+            if temp_df.shape[0] == 0:
                 return temp_df
             df = temp_df
         return df[self._owner]
+
+    def _get_all_possible_combination(self):
+        """
+        This method will return a list with all combination that is possible with the scale and the number of parents the
+        owner has.
+        :return: list of combinations
+        """
+        combination_list = []
+        for i in list(it.product(self._scale, repeat=len(self._parents))):
+            combination_list.append(i)
+
+        return combination_list
+
+    def _add_probabilities_to_row(self, entry_list):
+        """
+        This method will calculate the probability for each value in the scale
+        :param entry_list:
+        :return: list
+        """
+        row = []
+        for i in range(0, len(self._scale)):
+            probability = self._calculate_probability(entry_list, self._scale[i])
+            row.append(probability)
+        return row
--- bayesian_network/bayesian_network/conditional_probability_table/test/test_CPTBuilder.py
+++ bayesian_network/bayesian_network/conditional_probability_table/test/test_CPTBuilder.py
@@ -28,3 +28,35 @@ class TestCPTBuilder(TestCase):
         print(builder._apply_combination_filter(['F', 'T']))
         print(builder._apply_combination_filter(['A', 'B']))  # Combination is not apart of scale
 
+    def test_calculate_probability(self):
+        kb = KnowledgeBase('..\\..\\..\\test_data\\doug_example.csv', '..\\..\\..\\test_data\\doug_example.csv')
+        data = kb.get_query(['Success', 'Smart', 'Work Hard'])
+
+        builder = CPTBuilder(data, kb.get_scale())
+
+        fd = list(builder._apply_combination_filter(['T', 'T']))
+        self.assertTrue(.8 == builder._calculate_probability(fd, 'T'))
+
+        fd = list(builder._apply_combination_filter(['T', 'C']))
+        self.assertTrue(0 == builder._calculate_probability(fd, 'T'))
+
+        kb1 = KnowledgeBase('..\\..\\..\\test_data\\doug_example.csv', '..\\..\\..\\test_data\\doug_example.csv')
+        data = kb1.get_query(['Work Hard'])
+
+        builder1 = CPTBuilder(data, kb.get_scale())
+        self.assertTrue((11/24) == builder1._calculate_probability(list(data.values), 'T'))
+        self.assertTrue((13/24) == builder1._calculate_probability(list(data.values), 'F'))
+
+    def test_build_with_no_parents(self):
+        kb = KnowledgeBase('..\\..\\..\\test_data\\doug_example.csv', '..\\..\\..\\test_data\\doug_example.csv')
+        data = kb.get_query(['Work Hard'])
+        builder = CPTBuilder(data, kb.get_scale())
+
+        print(builder.build_with_no_parents())
+
+        kb1 = KnowledgeBase('..\\..\\..\\test_data\\doug_example.csv', '..\\..\\..\\test_data\\doug_example.csv')
+        data = kb1.get_query(['Smart'])
+        builder1 = CPTBuilder(data, kb.get_scale())
+
+        print(builder1.build_with_no_parents())
+
