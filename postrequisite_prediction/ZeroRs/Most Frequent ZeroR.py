1edfbd3ff93b2ee2a1d86daf00b0ca9b21ff6480
Chris kott <kottc@uwplatt.edu>
2020-06-02 12:31:40 -0500

    Commenting and some personal refactoring to play with things

--- postrequisite_prediction/ZeroRs/Most Frequent ZeroR.py
+++ postrequisite_prediction/ZeroRs/Most Frequent ZeroR.py
@@ -1,13 +1,12 @@
 import os
-
-import numpy as np
-np.random.seed(313131)
 import pandas as pd
-from random import choices
+import numpy as np
 import random
-import sys
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, roc_auc_score, accuracy_score
+# from random import choices
+# import sys
+# from sklearn.metrics import confusion_matrix, precision_score, recall_score, roc_auc_score, accuracy_score
 
+np.random.seed(313131)
 GRAPH_FILE_PREFIX = 'graph_term_'
 PREREQ_PROCESS_TYPE = 'All'
 STRATIFIED_DATA_PATH = '..\\data\\' + PREREQ_PROCESS_TYPE + 'PrereqFolds\\'
@@ -18,11 +17,12 @@ random.seed = 313131
 population = [0, 1]
 possible_grades = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-flatten = lambda l: [item for sublist in l for item in sublist]
+# flatten = lambda l: [item for sublist in l for item in sublist]  # Unused?
 
 
+# Grabs the training and testing data the given course name and fold number.
 def get_training_testing(course_name, number_of_fold):
-    return pd.read_csv(STRATIFIED_DATA_PATH + course_name + '_train_' + str(number_of_fold) + '.csv'),\
+    return pd.read_csv(STRATIFIED_DATA_PATH + course_name + '_train_' + str(number_of_fold) + '.csv'), \
            pd.read_csv(STRATIFIED_DATA_PATH + course_name + '_test_' + str(number_of_fold) + '.csv')
 
 
@@ -56,28 +56,40 @@ if __name__ == "__main__":
 
     prediction_array = None
     for each_course in os.listdir(TUNING_FILE_PATH):
-        each_course = each_course[:-4]
+        each_course = each_course[:-4]  # Get the name of the courses from the .npy files and drop the extension.
+
         try:
             train, test = get_training_testing(each_course, 1)
         except Exception as e:
             continue
+
         prediction_array = np.zeros(0)
         test_total = None
         for number_for_fold in range(1, 6):
+
             try:
                 train, test = get_training_testing(each_course, number_for_fold)
                 y_train = train[each_course].values
                 y_test = test[each_course].values
-                grades_distribution = dict.fromkeys(possible_grades)
 
+                # Grab all possible grades and populate the grades_distribution dictionary with zeros.
+                grades_distribution = dict.fromkeys(possible_grades)
                 for key, value in grades_distribution.items():
                     grades_distribution[key] = 0
+
+                # Increment each grade for each entry
                 for each_grade in y_train:
                     grades_distribution[each_grade] = grades_distribution[each_grade] + 1
+
+                # Find the grade with the most entries.
                 prediction = max(grades_distribution, key=grades_distribution.get)
 
+                # Create an array with the shape of y_test, but prediction in all of its values.
                 for_conca = np.full_like(y_test, prediction)
+
+                # Concatenate shaped prediction array onto prediction_array.
                 prediction_array = np.concatenate((prediction_array, for_conca), axis=0)
+
             except Exception as e:
                 break
 
@@ -88,10 +100,16 @@ if __name__ == "__main__":
             else:
                 test_total = pd.concat([test_total, test], axis=0, ignore_index=True)
 
-        predictions = pd.DataFrame.from_records(prediction_array.reshape(-1,1), columns=['prediction'])
+        # Make predictions a single vertical array and add it to pandas.
+        predictions = pd.DataFrame.from_records(prediction_array.reshape(-1, 1), columns=['prediction'])
+        # Concatenate test_total onto predictions.
         predictions = pd.concat([test_total, predictions], axis=1)
+        # Write to csv file.
         predictions.to_csv(RESULTS_FOLDER + str(each_course) + '.csv', index=False)
+        # Add to the big_predictions list.
         big_predictions += list(predictions['prediction'].values)
+
+    # Compile list of all predictions, and write to file.
     converted_grades = []
     for grade in big_predictions:
         converted_grades.append(reverse_convert_grade(grade))
