6f983a3b144dab3c34e08745f7817b5b661b7e30
Chris kott <kottc@uwplatt.edu>
2020-06-02 15:26:26 -0500

    Reformatted program output to be consistent between zeror and predict

--- postrequisite_prediction/ZeroRs/Most Frequent ZeroR.py
+++ postrequisite_prediction/ZeroRs/Most Frequent ZeroR.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import random
+
 # from random import choices
 # import sys
 # from sklearn.metrics import confusion_matrix, precision_score, recall_score, roc_auc_score, accuracy_score
@@ -16,6 +17,7 @@ TUNING_FILE_PATH = '..\\TuningResults\\' + PREREQ_PROCESS_TYPE + '\\LR\\'
 random.seed = 313131
 population = [0, 1]
 possible_grades = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
 
 # flatten = lambda l: [item for sublist in l for item in sublist]  # Unused?
 
@@ -52,7 +54,7 @@ def reverse_convert_grade(int_grade):
 
 
 if __name__ == "__main__":
-    big_predictions = []
+    big_predictions = None
 
     prediction_array = None
     for each_course in os.listdir(TUNING_FILE_PATH):
@@ -85,10 +87,10 @@ if __name__ == "__main__":
                 prediction = max(grades_distribution, key=grades_distribution.get)
 
                 # Create an array with the shape of y_test, but prediction in all of its values.
-                for_conca = np.full_like(y_test, prediction)
+                for_concat = np.full_like(y_test, prediction)
 
                 # Concatenate shaped prediction array onto prediction_array.
-                prediction_array = np.concatenate((prediction_array, for_conca), axis=0)
+                prediction_array = np.concatenate((prediction_array, for_concat), axis=0)
 
             except Exception as e:
                 break
@@ -101,18 +103,22 @@ if __name__ == "__main__":
                 test_total = pd.concat([test_total, test], axis=0, ignore_index=True)
 
         # Make predictions a single vertical array and add it to pandas.
-        predictions = pd.DataFrame.from_records(prediction_array.reshape(-1, 1), columns=['prediction'])
+        predictions = pd.DataFrame.from_records(prediction_array.reshape(-1, 1), columns=['predicted'])
         # Concatenate test_total onto predictions.
         predictions = pd.concat([test_total, predictions], axis=1)
         # Write to csv file.
         predictions.to_csv(RESULTS_FOLDER + str(each_course) + '.csv', index=False)
         # Add to the big_predictions list.
-        big_predictions += list(predictions['prediction'].values)
+        actual_data_frame = pd.DataFrame(test_total[each_course].values, columns=['actual'])
+        big_predictions = pd.concat([big_predictions, pd.concat([predictions['predicted'],
+                                                                 pd.DataFrame(test_total[each_course].values,
+                                                                              columns=['actual'])], axis=1)], axis=0)
 
     # Compile list of all predictions, and write to file.
-    converted_grades = []
-    for grade in big_predictions:
-        converted_grades.append(reverse_convert_grade(grade))
-    all_predictions = pd.DataFrame(converted_grades, columns=['predictions'])
-    all_predictions.to_csv(RESULTS_FOLDER + 'ALL_PREDICTIONS' + '.csv', index=False)
+    all_predictions = np.empty((0, 2))
+    for actual in big_predictions.itertuples():
+        all_predictions = np.vstack(
+            (all_predictions, [reverse_convert_grade(actual.predicted), reverse_convert_grade(actual.actual)]))
+    pd.DataFrame(all_predictions, columns=['predicted', 'actual']).to_csv(RESULTS_FOLDER + 'ALL_PREDICTIONS' + '.csv',
+                                                                          index=False)
     print('Done')
