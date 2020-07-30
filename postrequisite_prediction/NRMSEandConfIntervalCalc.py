d37fe65ab489992b83a7062b371669716bbdbf53
Chris kott <kottc@uwplatt.edu>
2020-07-30 13:23:48 -0500

    added confidence interval calculator, and reformatted folder a bit

--- postrequisite_prediction/NRMSEandConfIntervalCalc.py
+++ postrequisite_prediction/NRMSEandConfIntervalCalc.py
@@ -66,71 +66,46 @@ def convert_grade(string_grade):
 
 def set_paths():
     results_folder1 = Path('results/' + __tree_type.name + 'Prereq_' + __model_enumA.name + '_Results/')
-    results_folder2 = Path('results/' + __tree_type.name + 'Prereq_' + __model_enumB.name + '_Results/')
     if __model_enumA == __MODEL_TYPES_ENUM.BAYESIAN_NETWORK:
         results_folder1 = Path('results/BayesNet')
-    elif __model_enumB == __MODEL_TYPES_ENUM.BAYESIAN_NETWORK:
-        results_folder2 = Path('results/BayesNet')
-    if not (os.path.exists(results_folder1) and os.path.exists(results_folder2)):
-        raise FileNotFoundError("One or both of the file paths don't exist.")
-    return results_folder1, results_folder2
+    if not (os.path.exists(results_folder1)):
+        raise FileNotFoundError("The file paths don't exist.")
+    return results_folder1
 
 
-def get_pval():
-    __results_folder_A, __results_folder_B = set_paths()
+def get_nrmse():
+    __results_folder_A = set_paths()
     if __model_enumA == __MODEL_TYPES_ENUM.BAYESIAN_NETWORK:
         results_A = pd.read_csv(__results_folder_A / 'ALL_COURSES_PREDICTIONS_BayesNet.csv')
     else:
         results_A = pd.read_csv(__results_folder_A / ('ALL_COURSES_PREDICTIONS_' + __tree_type.name + "_"
                                                       + __model_enumA.name + '.csv'))
-    if __model_enumB == __MODEL_TYPES_ENUM.BAYESIAN_NETWORK:
-        results_B = pd.read_csv(__results_folder_B / 'ALL_COURSES_PREDICTIONS_BayesNet.csv')
-    else:
-        results_B = pd.read_csv(__results_folder_B / ('ALL_COURSES_PREDICTIONS_' + __tree_type.name + "_"
-                                                      + __model_enumB.name + '.csv'))
     results_A['actual'] = results_A['actual'].apply(convert_grade)
     results_A['predicted'] = results_A['predicted'].apply(convert_grade)
     results_A['diff^2'] = (results_A['actual'] - results_A['predicted']) ** 2
 
-    results_B['actual'] = results_B['actual'].apply(convert_grade)
-    results_B['predicted'] = results_B['predicted'].apply(convert_grade)
-    results_B['diff^2'] = (results_B['actual'] - results_B['predicted']) ** 2
-
     nrmses_a = np.sqrt(results_A.groupby('student_id').sum()['diff^2'] / results_A.groupby('student_id').size()) / 10
     nrmses_a = pd.DataFrame({'student_id': nrmses_a.index, __model_enumA.name: nrmses_a.values})
 
-    nrmses_b = np.sqrt(results_B.groupby('student_id').sum()['diff^2'] / results_B.groupby('student_id').size()) / 10
-    nrmses_b = pd.DataFrame({'student_id': nrmses_b.index, __model_enumB.name: nrmses_b.values})
-
-    total = pd.merge(nrmses_a, nrmses_b, "inner", "student_id")
-    if total[__model_enumA.name].mean() <= total[__model_enumB.name].mean():
-        better_model = __model_enumA.name
-        worse_model = __model_enumB.name
-    else:
-        better_model = __model_enumB.name
-        worse_model = __model_enumA.name
-    total['diff'] = total[better_model] - total[worse_model]
-    bootstrapped_values = Parallel(n_jobs=-1)(delayed(resample)(total['diff'].values, random_state=__RANDOM_SEED + i)
+    bootstrapped_values = Parallel(n_jobs=-1)(delayed(resample)(nrmses_a[__model_enumA.name].values, random_state=__RANDOM_SEED + i)
                                               for i in range(250000))
-    bootstrap = pd.DataFrame(bootstrapped_values)
-    bootstrap = bootstrap.T
+    bootstrap = pd.DataFrame(bootstrapped_values).T
 
-    bootstrap_means = bootstrap.mean().to_frame(name='mean_diff')
-    p_val = bootstrap_means[bootstrap_means['mean_diff'] > 0].count().values[0] / bootstrap_means['mean_diff'].count()
-    return better_model, worse_model, p_val
+    bootstrap_means = bootstrap.mean().to_frame(name='nrmses')
+    sorted_bootstrap_means = sorted(bootstrap_means['nrmses'].values)
+
+    return __model_enumA.name, nrmses_a[__model_enumA.name].mean(), sorted_bootstrap_means[6249], sorted_bootstrap_means[231249]
 
 
 if __name__ == "__main__":
     for tree_type in __TREE_TYPES_ENUM:
-        p_values = []
+        nrmses = []
         print(tree_type.name)
-        for val in combinations(list(map(int, __MODEL_TYPES_ENUM)), 2):
-            if not ((val[0] == __MODEL_TYPES_ENUM.BAYESIAN_NETWORK or val[1] == __MODEL_TYPES_ENUM.BAYESIAN_NETWORK)
-                    and ((tree_type == __TREE_TYPES_ENUM.ROOT) or (tree_type == __TREE_TYPES_ENUM.IMMEDIATE))):
+        for val in __MODEL_TYPES_ENUM:
+            if not ((val == __MODEL_TYPES_ENUM.BAYESIAN_NETWORK) and ((tree_type == __TREE_TYPES_ENUM.ROOT) or (tree_type == __TREE_TYPES_ENUM.IMMEDIATE))):
                 __tree_type = tree_type
-                __model_enumA = __MODEL_TYPES_ENUM(val[0])
-                __model_enumB = __MODEL_TYPES_ENUM(val[1])
-                p_val = get_pval()
-                p_values.append(p_val)
-                print(p_val)
-        pd.DataFrame(p_values, columns=['better', 'worse', 'p_value']).to_csv(Path('results/') / (tree_type.name + '_PValues.csv'), index=False)
+                __model_enumA = val
+                value = get_nrmse()
+                nrmses.append(value)
+                print(value)
+        pd.DataFrame(nrmses, columns=['model', 'NRMSE', 'lower_bound', 'upper_bound']).to_csv(Path('results/') / (tree_type.name + '_NRMSEs.csv'), index=False)
